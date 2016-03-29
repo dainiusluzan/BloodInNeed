d0218977a77fcec823801d9433bffa9121e7c557
Aivaras Baranauskas <aivaras.brn@gmail.com>
2016-03-30 00:16:49 +0300

    Pridėti Profile.show layout'ai, Translationai (nepajungti dar)

---
+++ src/UserBundle/Form/ProfileType.php
@@ -0,0 +1,34 @@
+<?php
+// src/UserBundle/Form/ProfileType.php
+
+namespace UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name');
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\ProfileFormType';
+
+        // Or for Symfony < 2.8
+        // return 'fos_user_registration';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_profile';
+    }
+
+    // For Symfony 2.x
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+}
