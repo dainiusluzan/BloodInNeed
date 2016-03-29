b4f326df4ae18fae14767a829ca7e5e6589c4894
Aivaras Baranauskas <aivaras.brn@gmail.com>
2016-03-29 23:35:29 +0300

    Pridėti RegistrationController ir RegistrationType. Pridėtas 'name' laukas į registraciją.

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -5,6 +5,7 @@ namespace UserBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -19,10 +20,35 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Please enter your name.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="The name is too short.",
+     *     maxMessage="The name is too long.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $name;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
         $this->roles = array('ROLE_USER');
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
     }
 }
