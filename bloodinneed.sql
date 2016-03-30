8a3ee28ec9e8d05e1be273f7dcd364be3f285606
Aivaras Baranauskas <aivaras.brn@gmail.com>
2016-03-30 16:16:50 +0300

    Atnaujinta duombazė.

--- bloodinneed.sql
+++ bloodinneed.sql
@@ -40,10 +40,17 @@ CREATE TABLE `fos_user` (
   `roles` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:array)',
   `credentials_expired` tinyint(1) NOT NULL,
   `credentials_expire_at` datetime DEFAULT NULL,
+  `first_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
+  `age` int(11) NOT NULL,
+  `blood_type` varchar(3) COLLATE utf8_unicode_ci NOT NULL,
+  `rh_factor` varchar(3) COLLATE utf8_unicode_ci NOT NULL,
+  `town` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_957A647992FC23A8` (`username_canonical`),
   UNIQUE KEY `UNIQ_957A6479A0D96FBF` (`email_canonical`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -52,7 +59,7 @@ CREATE TABLE `fos_user` (
 
 LOCK TABLES `fos_user` WRITE;
 /*!40000 ALTER TABLE `fos_user` DISABLE KEYS */;
-INSERT INTO `fos_user` VALUES (1,'test','test','test@test.com','test@test.com',1,'humsm9zi5qo880wcw0cg40okwo40ocg','$2y$13$humsm9zi5qo880wcw0cg4uvnBCinioNTDXKLjzLWsoq06eoJ/c.fW','2016-03-29 14:43:20',0,0,NULL,NULL,NULL,'a:1:{i:0;s:9:\"ROLE_USER\";}',0,NULL),(4,'admin_test','admin_test','admin@test.com','admin@test.com',1,'6p6prfj1h1c0wggk4wsw0g04woggcos','$2y$13$6p6prfj1h1c0wggk4wsw0e3cQk03evCAu12ebCMCRUaou5Z8UF9BO',NULL,0,0,NULL,NULL,NULL,'a:2:{i:0;s:9:\"ROLE_USER\";i:1;s:10:\"ROLE_ADMIN\";}',0,NULL),(5,'super_test','super_test','super@test.com','super@test.com',1,'5thurwcpxa0wwo088kkowoo400kk40g','$2y$13$5thurwcpxa0wwo088kkoweXD6JVRhsW8NW3NsBUFB7RbGbZsTXccC',NULL,0,0,NULL,NULL,NULL,'a:2:{i:0;s:9:\"ROLE_USER\";i:1;s:10:\"ROLE_SUPER\";}',0,NULL);
+INSERT INTO `fos_user` VALUES (1,'test','test','test@test.com','test@test.com',1,'humsm9zi5qo880wcw0cg40okwo40ocg','$2y$13$humsm9zi5qo880wcw0cg4uvnBCinioNTDXKLjzLWsoq06eoJ/c.fW','2016-03-30 13:20:08',0,0,NULL,NULL,NULL,'a:1:{i:0;s:9:\"ROLE_USER\";}',0,NULL,'Testas','Testinis','+37065214521',18,'A+','Rh+',0),(4,'admin_test','admin_test','admin@test.com','admin@test.com',1,'6p6prfj1h1c0wggk4wsw0g04woggcos','$2y$13$6p6prfj1h1c0wggk4wsw0e3cQk03evCAu12ebCMCRUaou5Z8UF9BO',NULL,0,0,NULL,NULL,NULL,'a:2:{i:0;s:9:\"ROLE_USER\";i:1;s:10:\"ROLE_ADMIN\";}',0,NULL,'','','',0,'','',0),(5,'super_test','super_test','super@test.com','super@test.com',1,'5thurwcpxa0wwo088kkowoo400kk40g','$2y$13$5thurwcpxa0wwo088kkoweXD6JVRhsW8NW3NsBUFB7RbGbZsTXccC',NULL,0,0,NULL,NULL,NULL,'a:2:{i:0;s:9:\"ROLE_USER\";i:1;s:16:\"ROLE_SUPER_ADMIN\";}',0,NULL,'','','',0,'','',0);
 /*!40000 ALTER TABLE `fos_user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -65,4 +72,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-03-29 16:55:54
+-- Dump completed on 2016-03-30 16:16:13
