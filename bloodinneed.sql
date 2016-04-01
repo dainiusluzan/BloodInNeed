9ca0b6350fcab0cb51d4197efbbcbc0a209804a8
Aivaras Baranauskas <aivaras.brn@gmail.com>
2016-04-01 15:56:35 +0300

    Atnaujinta duomenu baze.

--- bloodinneed.sql
+++ bloodinneed.sql
@@ -24,6 +24,7 @@ DROP TABLE IF EXISTS `fos_user`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `fos_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `townId` int(11) DEFAULT NULL,
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `username_canonical` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -45,12 +46,12 @@ CREATE TABLE `fos_user` (
   `phone` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
   `age` int(11) NOT NULL,
   `blood_type` varchar(3) COLLATE utf8_unicode_ci NOT NULL,
-  `rh_factor` varchar(3) COLLATE utf8_unicode_ci NOT NULL,
-  `town` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_957A647992FC23A8` (`username_canonical`),
-  UNIQUE KEY `UNIQ_957A6479A0D96FBF` (`email_canonical`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  UNIQUE KEY `UNIQ_957A6479A0D96FBF` (`email_canonical`),
+  KEY `IDX_957A6479E5D621EA` (`townId`),
+  CONSTRAINT `FK_957A6479E5D621EA` FOREIGN KEY (`townId`) REFERENCES `towns` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -59,7 +60,7 @@ CREATE TABLE `fos_user` (
 
 LOCK TABLES `fos_user` WRITE;
 /*!40000 ALTER TABLE `fos_user` DISABLE KEYS */;
-INSERT INTO `fos_user` VALUES (1,'test','test','test@test.com','test@test.com',1,'humsm9zi5qo880wcw0cg40okwo40ocg','$2y$13$humsm9zi5qo880wcw0cg4uvnBCinioNTDXKLjzLWsoq06eoJ/c.fW','2016-03-31 12:29:38',0,0,NULL,NULL,NULL,'a:1:{i:0;s:9:\"ROLE_USER\";}',0,NULL,'Testas','Testinis','+37065214521',18,'a-','rh+',0),(4,'admin_test','admin_test','admin@test.com','admin@test.com',1,'6p6prfj1h1c0wggk4wsw0g04woggcos','$2y$13$6p6prfj1h1c0wggk4wsw0e3cQk03evCAu12ebCMCRUaou5Z8UF9BO',NULL,0,0,NULL,NULL,NULL,'a:2:{i:0;s:9:\"ROLE_USER\";i:1;s:10:\"ROLE_ADMIN\";}',0,NULL,'','','',0,'','',0),(5,'super_test','super_test','super@test.com','super@test.com',1,'5thurwcpxa0wwo088kkowoo400kk40g','$2y$13$5thurwcpxa0wwo088kkoweXD6JVRhsW8NW3NsBUFB7RbGbZsTXccC',NULL,0,0,NULL,NULL,NULL,'a:2:{i:0;s:9:\"ROLE_USER\";i:1;s:16:\"ROLE_SUPER_ADMIN\";}',0,NULL,'','','',0,'','',0),(11,'aiarasb','aiarasb','aiarasb@gmail.com','aiarasb@gmail.com',1,'lqph7lqesf4k4ccos84ggwooco8scg8','$2y$13$lqph7lqesf4k4ccos84ggu7Crd3mVuZgHAoaNKS.8HLF44zMNXy3a','2016-04-01 14:52:02',0,0,NULL,NULL,NULL,'a:1:{i:0;s:9:\"ROLE_USER\";}',0,NULL,'Aivaras','Baranauskas','+37069669040',20,'a-','rh+',3);
+INSERT INTO `fos_user` VALUES (2,1,'test','test','test@test.com','test@test.com',1,'nm0mpc2u0o0k048c4g0s80w0os000gk','$2y$13$nm0mpc2u0o0k048c4g0s8uVBCXrc2bDzTIKCjvRs/.F.7HoPatyMy','2016-04-01 15:51:24',0,0,NULL,NULL,NULL,'a:1:{i:0;s:9:\"ROLE_USER\";}',0,NULL,'test','test','+37064521523',20,'A+'),(3,1,'admin_test','admin_test','admin@test.test','admin@test.test',1,'igdmt814hds0g080ckckgos008o4oc0','$2y$13$igdmt814hds0g080ckckgeZMMqP5KkFv0MQF/6EinT3Ie8/zp7wmi','2016-04-01 15:52:17',0,0,NULL,NULL,NULL,'a:2:{i:0;s:9:\"ROLE_USER\";i:1;s:10:\"ROLE_ADMIN\";}',0,NULL,'admin','test','+37054215268',21,'AB+'),(4,1,'super_test','super_test','super@test.test','super@test.test',1,'pvyemk231pck4oogs4o0cs88swwggkc','$2y$13$pvyemk231pck4oogs4o0ceCEzN3D5YWtLSDCGuLeyV9IVnDSngWLe','2016-04-01 15:54:33',0,0,NULL,NULL,NULL,'a:2:{i:0;s:9:\"ROLE_USER\";i:1;s:16:\"ROLE_SUPER_ADMIN\";}',0,NULL,'super','test','+37054251253',22,'O-');
 /*!40000 ALTER TABLE `fos_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -72,9 +73,9 @@ DROP TABLE IF EXISTS `towns`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `towns` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `town` varchar(255) NOT NULL,
+  `town` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -96,4 +97,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-04-01 15:08:55
+-- Dump completed on 2016-04-01 15:56:19
