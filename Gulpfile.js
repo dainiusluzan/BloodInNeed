a8717b01e53fb6e340bb162de3f92f7721cfd1a4
Marius Valasevicius <marius.valasevicius@gmail.com>
2016-04-02 16:44:34 +0300

    Sukonfiguruota gulp taskai, prideti pagrindiniai css, js folderiai, pakeista base layout ir homepage layout

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,8 @@ var gulp    = require('gulp');
 var sass    = require('gulp-sass');
 var concat  = require('gulp-concat');
 var uglify  = require('gulp-uglify');
+var uglifycss  = require('gulp-uglifycss');
+var gulpif  = require('gulp-if');
 
 var dir = {
     assets: './app/Resources/',
@@ -12,21 +14,41 @@ var dir = {
     bootstrapJS: './bower_components/bootstrap-sass/assets/javascripts/bootstrap/'
 };
 
-gulp.task('sass', function() {
-    gulp.src(dir.assets + 'style/main.scss')
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+/*gulp.task('sass', function() {
+    gulp.src([
+        dir.assets + 'style/main.scss',
+        dir.bower +  'bootstrap/dist/css/bootstrap.css'
+        ])
+        .pipe(gulpif(/[.]scss/, sass({ outputStyle: 'compressed' }).on('error', sass.logError)))
+        .pipe(sass())
         .pipe(concat('style.css'))
         .pipe(gulp.dest(dir.dist + 'css'));
+});*/
+
+//CSS TASK: write one minified css file out of bootstrap.less and all of my custom less files
+gulp.task('sass', function () {
+    return gulp.src([
+            dir.bower  + 'bootstrap/dist/css/bootstrap.css',
+           // dir.assets + 'style/*.scss',
+            dir.assets + 'style/scrolling-nav.scss'
+        ])
+        .pipe(gulpif(/[.]scss/, sass()))
+        .pipe(concat('styles.css'))
+        .pipe(uglifycss())
+        .pipe(gulp.dest(dir.dist + 'css'));
 });
+
 
 gulp.task('scripts', function() {
     gulp.src([
             dir.bower + 'jquery/dist/jquery.min.js',
+            dir.bower + 'bootstrap/dist/js/bootstrap.min.js',
             // Bootstrap JS modules
             //dir.bootstrapJS + 'transition.js',
             //...
             // Main JS file
-            dir.assets + 'scripts/main.js'
+            dir.assets + 'scripts/jquery.easing.min.js',
+            dir.assets + 'scripts/scrolling-nav.js'
         ])
         .pipe(concat('script.js'))
         .pipe(uglify())
